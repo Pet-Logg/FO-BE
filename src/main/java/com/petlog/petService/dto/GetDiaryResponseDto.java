3c70750375b58768131c971177fcf673bf9e86ff
Jeon Hyejin <hyejda00@naver.com>
2025-03-18 00:09:33 +0900

    Fix: getDiaryById 실행 시 imgUrl 배열로 모두 가져오도록 수정

--- src/main/java/com/petlog/petService/dto/GetDiaryResponseDto.java
+++ src/main/java/com/petlog/petService/dto/GetDiaryResponseDto.java
@@ -4,15 +4,16 @@ import com.petlog.petService.domain.Diary;
 import io.micrometer.common.lang.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@EqualsAndHashCode(callSuper = true)
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetDiaryResponseDto extends Diary {
 
     @Nullable
-    private String imgUrl;
+    private List<String> imgUrl;
+
 }
